98a766b92577d89ff2076c66070f14ef7507224e
Jaypal Solanki <jaypsl263@gmail.com>
2018-09-30 05:31:53 -0400

    added passport

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var express = require('express');
+var express = require('express'),
+mongoose = require('mongoose'),
+passport = require('passport'),
+bodyParser = require('body-parser'),
+localStrategy = require('passport-local'),
+passportLocalMongoose = require('passport-local-mongoose'),
+User = require('./models/user');
+
+mongoose.connect('mongodb://localhost/authentication_app');
+
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('express-session')({
+    secret : 'jay-auth-key',
+    resave : false,
+    saveUninitialized : false
+}))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req,res)=>{
     res.render('home');
@@ -10,6 +30,29 @@ app.get('/', (req,res)=>{
 app.get('/secret', (req,res)=> {
     res.render('secret');
 });
+
+//auth routes
+
+//sign up form
+app.get('/register', (req,res)=>{
+res.render('register');
+})
+
+//handle user sign up
+app.post('/register', (req,res)=> {
+    // res.send('register post route');
+    // req.body.username
+    // req.body.password
+    User.register(new User({username : req.body.username}), req.body.password, (err,user)=> {
+        if(err){
+            console.log(err);
+            res.render('register');
+        }
+        passport.authenticate('local')(req,res,()=>{
+            res.redirect('/secret')
+        })
+    })
+})
 
 app.listen(3000, ()=> {
     console.log('server is running at 3000')
